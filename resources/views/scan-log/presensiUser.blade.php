42b8260665492a66b58b2b2a2fa98ead0f966749
codeguna <pratamagunadhi@gmail.com>
2023-10-23 10:20:49 +0700

    fix UI Presensi Create

--- resources/views/scan-log/presensiUser.blade.php
+++ resources/views/scan-log/presensiUser.blade.php
@@ -1,12 +1,36 @@
 @extends('layouts.dashboard')
 
 @section('template_title')
-    Tenaga Kependidikan - Aktif
+    Lakukan Presensi
 @endsection
 
 @section('content')
-    <div class="container">
-        <h1>Presensi</h1>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h1><i class="fas fa-user-clock"></i> Presensi</h1>
+                        <div class="alert alert-warning" role="alert">
+                            <strong><i class="fa fa-info-circle" aria-hidden="true"></i> Tombol Presensi aktif di jam:
+                            </strong>
+                            <ul>
+                                <li>
+                                    11:00 - 12:00
+                                </li>
+                                <li>13:00 -
+                                    14:00</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <button id="presensiButton" class="btn btn-lg btn-success w-100">
+                            <i class="fa fa-check-circle" aria-hidden="true"></i> Lakukan Presensi
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
 
         @if (session('success'))
             <div class="alert alert-success">
@@ -19,48 +43,42 @@
                 {{ session('error') }}
             </div>
         @endif
+    </div>
+@endsection
 
-        <!-- Tombol Presensi -->
-        <button id="presensiButton">Presensi</button>
+@section('scripts')
+    <script>
+        function isButtonActive() {
+            const now = new Date();
+            const dayOfWeek = now.getDay(); // Hari dalam format 0 (Minggu) hingga 6 (Sabtu)
+            const hour = now.getHours();
 
+            // Aktifkan tombol pada Senin hingga Sabtu antara jam 11:00 - 12:00 dan 13:00 - 14:00
+            const isButtonEnabled = dayOfWeek >= 1 && dayOfWeek <= 6 &&
+                ((hour >= 11 && hour < 12) || (hour >= 13 && hour < 14));
 
-        <!-- Timer atau Jam Aktif -->
-        <p>Jam aktif: 11:00 - 12:00 dan 13:00 - 14:00</p>
-    @endsection
+            return isButtonEnabled;
+        }
 
-    @section('scripts')
-        <script>
-            function isButtonActive() {
-                const now = new Date();
-                const dayOfWeek = now.getDay(); // Hari dalam format 0 (Minggu) hingga 6 (Sabtu)
-                const hour = now.getHours();
+        function updateButtonStatus() {
+            const button = document.getElementById('presensiButton');
 
-                // Aktifkan tombol pada Senin hingga Sabtu antara jam 11:00 - 12:00 dan 13:00 - 14:00
-                const isButtonEnabled = dayOfWeek >= 1 && dayOfWeek <= 6 &&
-                    ((hour >= 11 && hour < 12) || (hour >= 13 && hour < 14));
-
-                return isButtonEnabled;
+            if (isButtonActive()) {
+                button.disabled = false;
+            } else {
+                button.disabled = true;
             }
+        }
 
-            function updateButtonStatus() {
-                const button = document.getElementById('presensiButton');
+        // Panggil updateButtonStatus saat halaman dimuat dan setiap menit untuk memperbarui status tombol
+        window.onload = updateButtonStatus;
+        setInterval(updateButtonStatus, 60000); // Setiap 1 menit
+    </script>
 
-                if (isButtonActive()) {
-                    button.disabled = false;
-                } else {
-                    button.disabled = true;
-                }
-            }
-
-            // Panggil updateButtonStatus saat halaman dimuat dan setiap menit untuk memperbarui status tombol
-            window.onload = updateButtonStatus;
-            setInterval(updateButtonStatus, 60000); // Setiap 1 menit
-        </script>
-
-        <script>
-            document.getElementById('presensiButton').addEventListener('click', function() {
-                // Lakukan redirect ke rute 'presensi' di Laravel
-                window.location.href = '{{ route('admin.presensi') }}';
-            });
-        </script>
-    @endsection
+    <script>
+        document.getElementById('presensiButton').addEventListener('click', function() {
+            // Lakukan redirect ke rute 'presensi' di Laravel
+            window.location.href = '{{ route('admin.presensi') }}';
+        });
+    </script>
+@endsection
