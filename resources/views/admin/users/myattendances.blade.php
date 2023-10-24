1e64cc86a9e58e636a7ba72fb1bea84ef8d749cb
codeguna <pratamagunadhi@gmail.com>
2023-10-24 10:35:13 +0700

    add validation date

--- resources/views/admin/users/myattendances.blade.php
+++ resources/views/admin/users/myattendances.blade.php
@@ -100,20 +100,24 @@
                         </h3>
                     </div>
                     <div class="card-body">
-                        <form action="{{ route('admin.scan-log.my-attendances-filter') }}" method="GET">
+                        <form action="{{ route('admin.scan-log.my-attendances-filter') }}" method="GET"
+                            id="attendanceForm">
                             <div class="row">
                                 <div class="col md-12">
                                     <h4 class="text-center">
                                         Tanggal Awal / Tanggal Akhir
                                     </h4>
                                     <div class="input-group">
-                                        <input class="form-control" type="date" name="start_date" required>
-                                        <input class="form-control" type="date" name="end_date" required>
+                                        <input class="form-control" type="date" name="start_date" id="start_date"
+                                            required>
+                                        <input class="form-control" type="date" name="end_date" id="end_date" required>
+                                        <p id="date_error" style="color: red;"></p>
                                         <span class="input-group-btn">
                                             <a href="{{ route('admin.scan-log.my-attendances') }}" class="btn btn-success">
                                                 <i class="fas fa-sync"></i>
                                             </a>
-                                            <button type="submit" class="btn btn-warning" type="button" aria-label="">
+                                            <button type="submit" class="btn btn-warning" type="button"
+                                                id="submit_button">
                                                 <i class="fa fa-search" aria-hidden="true"></i>
                                             </button>
                                         </span>
@@ -179,4 +183,21 @@
             </div>
         </div>
     </div>
+@endsection
+@section('scripts')
+    <script>
+        document.getElementById('attendanceForm').addEventListener('submit', function(e) {
+            e.preventDefault(); // Mencegah pengiriman formulir secara otomatis
+
+            var startDate = new Date(document.getElementById('start_date').value);
+            var endDate = new Date(document.getElementById('end_date').value);
+
+            if (startDate > endDate) {
+                alert("Tanggal Akhir tidak bisa lebih kecil dari Tanggal Awal.");
+            } else {
+                document.getElementById('date_error').textContent = "";
+                this.submit(); // Kirim formulir jika valid
+            }
+        });
+    </script>
 @endsection
