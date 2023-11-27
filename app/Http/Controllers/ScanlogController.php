95e2c0af4df3dcd14e1332ea58bf446b82ca4a59
codeguna <pratamagunadhi@gmail.com>
2023-11-27 10:14:39 +0700

    fix period myattendances

--- app/Http/Controllers/ScanlogController.php
+++ app/Http/Controllers/ScanlogController.php
@@ -291,11 +291,23 @@ class ScanlogController extends Controller
     public function myAttendances(){
 
         $pin = Auth::user()->pin;
-       // Hitung startDate (bulan lalu dengan tanggal 26)
-       $startDate = Carbon::now()->subMonth()->startOfMonth()->addDays(25);
+        // Hitung startDate (tanggal 26 bulan lalu)
+        $today = Carbon::now();
+        $startDate = Carbon::now()->subMonth()->startOfMonth()->addDays(25);
 
-       // Hitung endDate (bulan ini dengan tanggal 25)
-       $endDate = Carbon::now()->startOfMonth()->addDays(24);
+        // Peroleh tanggal 26 bulan lalu
+        $startDate = Carbon::now()->subMonth()->startOfMonth()->addDays(25);
+
+        // Peroleh tanggal 25 bulan ini jika bulan ini memiliki kurang dari 25 hari, jika tidak gunakan hari terakhir dari bulan ini
+        $currentDay = Carbon::now()->day;
+        $endDateDay = $currentDay >= 26 ? 25 : min($currentDay, 25);
+
+        // Periksa apakah bulan lalu memiliki 31 hari
+        $lastMonthDays = Carbon::now()->subMonth()->endOfMonth()->day;
+        $endDateDay = $lastMonthDays == 31 ? 26 : $endDateDay;
+
+        $endDate = Carbon::now()->startOfMonth()->addDays($endDateDay - 1);
+        
         //$today  = '2023/10/21';
         $user = Auth::user(); // Assuming you are using Laravel's built-in authentication
         $id = $user->pin;
