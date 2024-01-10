c977a36b44dec69eeaad439fee270d9b517d4f3d
codeguna <pratamagunadhi@gmail.com>
2024-01-10 14:12:40 +0700

    disable telat

--- resources/views/admin/users/myattendances.blade.php
+++ resources/views/admin/users/myattendances.blade.php
@@ -338,7 +338,7 @@
                 </div>
             </div>
         </div>
-        <div class="row">
+        {{-- <div class="row">
             <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">
@@ -423,7 +423,7 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> --}}
     </div>
 @endsection
 @section('scripts')
