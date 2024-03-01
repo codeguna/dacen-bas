ca894d83d8c17b62cd0256262e122693ba742ae3
codeguna <pratamagunadhi@gmail.com>
2024-03-01 11:00:07 +0700

    [FIX] bug when submit filtered dates

--- resources/views/scan-log/result-total-hours.blade.php
+++ resources/views/scan-log/result-total-hours.blade.php
@@ -10,7 +10,7 @@
             <div class="col-sm-12">
                 <div class="card">
                     <div class="card-body">
-                        <form action="{{ route('admin.scan-log.resultLate') }}" method="GET" id="attendanceForm">
+                        <form action="{{ route('admin.scan-log.result-total-hours') }}" method="GET" id="attendanceForm">
                             <div class="row">
                                 <div class="col md-12">
                                     <h3 class="text-center">
@@ -21,9 +21,9 @@
                                         Tanggal Awal / Tanggal Akhir
                                     </h4>
                                     <div class="input-group">
-                                        <input class="form-control" type="date" name="startSate" id="startSate"
+                                        <input class="form-control" type="date" name="startDate" id="startDate"
                                             value="{{ request('startDate') }}" required>
-                                        <input class="form-control" type="date" name="end_date" id="endDate"
+                                        <input class="form-control" type="date" name="endDate" id="endDate"
                                             value="{{ request('endDate') }}" required>
                                         <p id="date_error" style="color: red;"></p>
                                         <span class="input-group-btn">
@@ -164,8 +164,8 @@
         document.getElementById('attendanceForm').addEventListener('submit', function(e) {
             e.preventDefault(); // Mencegah pengiriman formulir secara otomatis
 
-            var startDate = new Date(document.getElementById('start_date').value);
-            var endDate = new Date(document.getElementById('end_date').value);
+            var startDate = new Date(document.getElementById('startDate').value);
+            var endDate = new Date(document.getElementById('endDate').value);
 
             if (startDate > endDate) {
                 alert("Tanggal Akhir tidak bisa lebih kecil dari Tanggal Awal.");
