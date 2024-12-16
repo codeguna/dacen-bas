69fcc276dbaba14cf5bbd95afd87b81f41bb5772
codeguna <pratamagunadhi@gmail.com>
2024-12-16 08:47:36 +0700

    [FIX] Stay on tab when refresh

--- resources/views/performance-appraisal/select-period.blade.php
+++ resources/views/performance-appraisal/select-period.blade.php
@@ -16,41 +16,41 @@
                         <!-- Nav tabs -->
                         <ul class="nav nav-tabs" id="myTab" role="tablist">
                             <li class="nav-item" role="presentation">
-                                <button class="nav-link active" id="home-tab" data-toggle="tab" data-target="#home"
-                                    type="button" role="tab" aria-controls="home" aria-selected="true">
-                                  <i class="fas fa-calendar-alt    "></i> Per Periode
+                                <button class="nav-link active" id="period-tab" data-toggle="tab" data-target="#period"
+                                    type="button" role="tab" aria-controls="period" aria-selected="true">
+                                    <i class="fas fa-calendar-alt"></i> Per Periode
                                 </button>
                             </li>
                             <li class="nav-item" role="presentation">
-                                <button class="nav-link" id="profile-tab" data-toggle="tab" data-target="#profile"
-                                    type="button" role="tab" aria-controls="profile" aria-selected="false">
-                                  <i class="fas fa-building"></i>  Per Departemen
+                                <button class="nav-link" id="department-tab" data-toggle="tab" data-target="#department"
+                                    type="button" role="tab" aria-controls="department" aria-selected="false">
+                                    <i class="fas fa-building"></i> Per Departemen
                                 </button>
                             </li>
                             <li class="nav-item" role="presentation">
-                                <button class="nav-link" id="messages-tab" data-toggle="tab" data-target="#messages"
-                                    type="button" role="tab" aria-controls="messages" aria-selected="false">
-                                   <i class="fas fa-user-plus"></i> Per Orang
+                                <button class="nav-link" id="person-tab" data-toggle="tab" data-target="#person"
+                                    type="button" role="tab" aria-controls="person" aria-selected="false">
+                                    <i class="fas fa-user-plus"></i> Per Orang
                                 </button>
                             </li>
                         </ul>
 
                         <!-- Tab panes -->
                         <div class="tab-content">
-                            <div class="tab-pane active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                            <div class="tab-pane active" id="period" role="tabpanel" aria-labelledby="period-tab">
                                 @include('performance-appraisal.pa.all-person')
                             </div>
-                            <div class="tab-pane" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                            <div class="tab-pane" id="department" role="tabpanel" aria-labelledby="department-tab">
                                 @include('performance-appraisal.pa.department')
                             </div>
-                            <div class="tab-pane" id="messages" role="tabpanel" aria-labelledby="messages-tab">
+                            <div class="tab-pane" id="person" role="tabpanel" aria-labelledby="person-tab">
                                 @include('performance-appraisal.pa.person')
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>        
+        </div>
     </div>
 @endsection
 @section('scripts')
@@ -76,6 +76,21 @@
                 "autoWidth": false,
                 "responsive": true,
             });
+        });
+
+        $(document).ready(function() {
+            // Simpan tab aktif di local storage saat tab diubah
+            $('button[data-toggle="tab"]').on('shown.bs.tab', function(e) {
+                localStorage.setItem('activeTab', $(e.target).data('target'));
+            });
+
+            // Muat tab aktif terakhir dari local storage
+            var activeTab = localStorage.getItem('activeTab');
+            if (activeTab) {
+                $('#myTab button[data-target="' + activeTab + '"]').tab('show');
+                $('.tab-content .tab-pane').removeClass('active'); // Hapus kelas active dari semua tab-pane
+                $(activeTab).addClass('active'); // Tambahkan kelas active ke tab-pane yang sesuai
+            }
         });
     </script>
 @endsection
